2439e9fa027955fd2b9ae731afe41fcea2a06bcb
AsyaP <anastasiapapina5232@gmail.com>
2019-06-03 20:53:38 +0300

    search

---
+++ front-end/app/src/main/java/com/example/s_mart/DataSearchInformation.java
@@ -0,0 +1,18 @@
+package com.example.s_mart;
+
+public class DataSearchInformation {
+    public static String[] id = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+            "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"};
+    public static String[] nameArray = {"Apple IPhone SE 32GB", "Apple IPhone XR 128GB", "Apple IPhone 6 32GB", "Apple IPhone X 64GB",
+            "Apple IPhone 8 64GB", "Apple IPhone XR 64GB", "Apple IPhone SE 64GB", "Apple IPhone 6S 32GB", "Apple IPhone X 256GB", "Apple IPhone 7 128GB",
+            "Apple IPhone 6 32GB", "Apple IPhone 7 Plus 64GB", "Apple IPhone XS 64GB","Apple IPhone 8 256GB", "Apple IPhone XS Max 64GB", "Apple IPhone SE 128GB",
+            "Apple IPhone XS Max 128GB", "Apple IPhone XR 256GB", "Apple IPhone 7 Plus 128GB","Apple IPhone 6S 64GB"};
+    public static String[] priceArray = {"17690", "55999", "19990", "54990", "39990", "51665", "25220", "23500", "67900", "35990", "19990", "38990", "66990", "46145",
+    "73990", "28169", "81990", "61990", "43990", "21510"};
+    public static String[] categoryArray = {"Смартфоны и связь","Смартфоны и связь", "Смартфоны и связь", "Смартфоны и связь",
+            "Смартфоны и связь","Смартфоны и связь", "Смартфоны и связь", "Смартфоны и связь", "Смартфоны и связь","Смартфоны и связь",
+            "Смартфоны и связь", "Смартфоны и связь", "Смартфоны и связь","Смартфоны и связь", "Смартфоны и связь", "Смартфоны и связь",
+            "Смартфоны и связь","Смартфоны и связь", "Смартфоны и связь", "Смартфоны и связь"};
+    public static String[] ratingArray = {"Средний", "Высокий", "Средний", "Низкий", "Высокий", "Высокий", "Низкий", "Средний", "Высокий",
+            "Низкий", "Средний", "Высокий", "Высокий", "Высокий", "Средний", "Средний", "Средний", "Высокий", "Высокий", "Низкий"};
+}
--- front-end/app/src/main/java/com/example/s_mart/HomeFragment.java
+++ front-end/app/src/main/java/com/example/s_mart/HomeFragment.java
@@ -2,9 +2,12 @@ package com.example.s_mart;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 public class HomeFragment extends Fragment{
 
     public HomeFragment() {
@@ -17,7 +20,30 @@ public class HomeFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        Button search = (Button) view.findViewById(R.id.search);
+        Button apply = (Button) view.findViewById(R.id.apply);
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new SearchFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.fl_content, fragment);
+                transaction.commit();
+            }
+
+        });
+        apply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new ApplyFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.fl_content, fragment);
+                transaction.commit();
+            }
+
+        });
+        return view;
     }
 
 }
